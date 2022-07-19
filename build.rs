f839ed66b6473926a679c6fc74c95cc0f34aa4f6
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2022-07-19 17:06:36 +0200

    Attach tracing programs to matching HID devices
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+extern crate bindgen;
+
 use libbpf_cargo::SkeletonBuilder;
 use std::fs;
 use std::process::Command;
@@ -5,10 +7,12 @@ use std::{env, path::Path, path::PathBuf};
 
 const DIR: &str = "./src/bpf/";
 const SRC: &str = "./src/bpf/attach.bpf.c";
+const WRAPPER: &str = "./src/attach_wrapper.h";
 const TARGET_DIR: &str = "./target";
 
 fn main() {
     println!("cargo:rerun-if-changed={}", DIR);
+    println!("cargo:rerun-if-changed={}", WRAPPER);
 
     let mut out =
         PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script"));
@@ -28,4 +32,26 @@ fn main() {
     let dest_path = Path::new(TARGET_DIR).join("bpf").join("attach.bpf.o");
 
     fs::remove_file(dest_path).unwrap();
+
+    // Create a wrapper around our bpf interface
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(WRAPPER)
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("attach_bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate libbpf_sys;
 extern crate mio;
 extern crate udev;
 
@@ -6,16 +7,21 @@ use glob::MatchOptions;
 use std::io;
 use std::path::PathBuf;
 
+include!(concat!(env!("OUT_DIR"), "/attach_bindings.rs"));
+
 mod attachbpf {
     include!(concat!(env!("OUT_DIR"), "/attach.skel.rs"));
 }
 
+use attachbpf::*;
+
 mod poll {
     use std::io;
 
+    use crate::attachbpf::*;
     use mio::{Events, Interest, Poll, Token};
 
-    pub fn poll(mut socket: udev::MonitorSocket) -> io::Result<()> {
+    pub fn poll(mut socket: udev::MonitorSocket, skel: &AttachSkel) -> io::Result<()> {
         let mut poll = Poll::new()?;
         let mut events = Events::with_capacity(1024);
 
@@ -30,7 +36,7 @@ mod poll {
 
             for event in &events {
                 if event.token() == Token(0) && event.is_writable() {
-                    socket.clone().for_each(|x| super::print_event(x));
+                    socket.clone().for_each(|x| super::print_event(x, skel));
                 }
             }
         }
@@ -68,7 +74,46 @@ fn get_bpffs_path(device: &udev::Device) -> String {
     )
 }
 
-fn load_bpf(device: &udev::Device, path: PathBuf) -> Result<(), libbpf_rs::Error> {
+fn attach_bpf(
+    device: &udev::Device,
+    skel: &AttachSkel,
+    prog: &libbpf_rs::Program,
+) -> Result<(), libbpf_rs::Error> {
+    let fd = skel.progs().attach_prog().fd();
+    let hid_sys = device.sysname().to_str().unwrap();
+    let hid_id = u32::from_str_radix(&hid_sys[15..], 16).unwrap();
+
+    let attach_args = attach_prog_args {
+        prog_fd: prog.fd(),
+        hid: hid_id,
+        retval: -1,
+    };
+    let attach_args_ptr: *const libc::c_void = &attach_args as *const _ as *const libc::c_void;
+    let mut run_opts = libbpf_sys::bpf_test_run_opts::default();
+
+    run_opts.sz = std::mem::size_of::<libbpf_sys::bpf_test_run_opts>() as u64;
+    run_opts.ctx_in = attach_args_ptr;
+    run_opts.ctx_size_in = std::mem::size_of::<attach_prog_args>() as u32;
+
+    let run_opts_ptr: *mut libbpf_sys::bpf_test_run_opts = &mut run_opts;
+
+    let err = unsafe { libbpf_sys::bpf_prog_test_run_opts(fd, run_opts_ptr) };
+
+    println!(
+        "attached {} to device id {} through fd {} err: {}",
+        prog.name(),
+        hid_id,
+        fd,
+        err,
+    );
+    Ok(())
+}
+
+fn load_bpf(
+    device: &udev::Device,
+    path: PathBuf,
+    skel: &AttachSkel,
+) -> Result<(), libbpf_rs::Error> {
     println!("found BPF object at {:?}", path.display());
 
     let mut obj_builder = libbpf_rs::ObjectBuilder::default();
@@ -79,6 +124,11 @@ fn load_bpf(device: &udev::Device, path: PathBuf) -> Result<(), libbpf_rs::Error
 
     for prog in object.progs_iter_mut() {
         println!("found prog {} of type {}", prog.name(), prog.prog_type(),);
+
+        match prog.prog_type() {
+            libbpf_rs::ProgramType::Tracing => attach_bpf(device, skel, prog)?,
+            _ => (),
+        }
 
         let path = format!("{}/{}", get_bpffs_path(device), prog.name(),);
 
@@ -101,7 +151,7 @@ fn bump_memlock_rlimit() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn add_device(device: udev::Device) {
+fn add_device(device: udev::Device, skel: &AttachSkel) {
     let prefix = get_filename(&device);
 
     println!(
@@ -120,7 +170,7 @@ fn add_device(device: udev::Device) {
 
     for entry in glob_with(&glob_path[..], options).unwrap() {
         if let Ok(path) = entry {
-            load_bpf(&device, path).unwrap_or(());
+            load_bpf(&device, path, skel).unwrap_or(());
         }
     }
 }
@@ -133,15 +183,15 @@ fn remove_device(device: udev::Device) {
     std::fs::remove_dir_all(path).unwrap_or(())
 }
 
-fn handle_event(event_type: udev::EventType, device: udev::Device) {
+fn handle_event(event_type: udev::EventType, device: udev::Device, skel: &AttachSkel) {
     match event_type {
-        udev::EventType::Add => add_device(device),
+        udev::EventType::Add => add_device(device, skel),
         udev::EventType::Remove => remove_device(device),
         _ => (),
     }
 }
 
-fn print_event(event: udev::Event) {
+fn print_event(event: udev::Event, skel: &AttachSkel) {
     println!(
         "{}: {} {} (subsystem={}, sysname={})",
         event.sequence_number(),
@@ -153,11 +203,17 @@ fn print_event(event: udev::Event) {
         event.sysname().to_str().unwrap_or(""),
     );
 
-    handle_event(event.event_type(), event.device())
+    handle_event(event.event_type(), event.device(), skel)
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), io::Error> {
+    let skel_builder = AttachSkelBuilder::default();
+
     bump_memlock_rlimit()?;
+
+    let open_skel = skel_builder.open().unwrap();
+
+    let skel = open_skel.load().unwrap();
 
     let socket = udev::MonitorBuilder::new()?
         .match_subsystem("hid")?
@@ -168,8 +224,8 @@ fn main() -> io::Result<()> {
     enumerator.match_subsystem("hid").unwrap();
 
     for device in enumerator.scan_devices().unwrap() {
-        handle_event(udev::EventType::Add, device);
+        handle_event(udev::EventType::Add, device, &skel);
     }
 
-    poll::poll(socket)
+    poll::poll(socket, &skel)
 }
