b55c3696a365b0c01f35a3871ce58625be2f0087
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2022-07-20 19:28:35 +0200

    Add 2 demonstration programs
    The programs are not loaded unless we comment a line in probe()
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

---
+++ src/bpf/b0003g0001v*p*-debug-usb.bpf.c
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2022 Benjamin Tissoires
+ */
+
+#include "vmlinux.h"
+#include "hid_bpf_helpers.h"
+#include <bpf/bpf_tracing.h>
+
+char str[64];
+
+SEC("fmod_ret/hid_bpf_device_event")
+int BPF_PROG(trace_usb, struct hid_bpf_ctx *hid_ctx)
+{
+	int i, j;
+	__u8 *data;
+
+	bpf_printk("event: size: %d", hid_ctx->size);
+	for (i = 0; i * 64 < hid_ctx->size && i < 64; i++) {
+		data = hid_bpf_get_data(hid_ctx, i * 64, 64);
+		if (!data)
+			return 0; /* EPERM check */
+
+		for (j = 0; j < 8 && i * 64 + j * 8 < hid_ctx->size; j++) {
+			long n;
+
+			n = BPF_SNPRINTF(str, sizeof(str),
+					 "%02x %02x %02x %02x %02x %02x %02x %02x ",
+					 data[j * 8],
+					 data[j * 8 + 1],
+					 data[j * 8 + 2],
+					 data[j * 8 + 3],
+					 data[j * 8 + 4],
+					 data[j * 8 + 5],
+					 data[j * 8 + 6],
+					 data[j * 8 + 7]
+					 );
+
+			bpf_printk(" 0x%08x: %s", i * 64 + j * 8, str);
+		}
+	}
+
+	return 0;
+}
+
+SEC("syscall")
+int probe(struct probe_args *ctx)
+{
+	ctx->retval = 0;
+
+	/* comment the following line to actually bind the program */
+	ctx->retval = -22;
+
+	return 0;
+}
+char _license[] SEC("license") = "GPL";
---
+++ src/bpf/b0003g0001v04d9pa09f-mouse.bpf.c
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2022 Benjamin Tissoires
+ */
+
+#include "vmlinux.h"
+#include "hid_bpf_helpers.h"
+#include <bpf/bpf_tracing.h>
+
+SEC("fmod_ret/hid_bpf_device_event")
+int BPF_PROG(hid_y_event, struct hid_bpf_ctx *hctx)
+{
+	s16 y;
+	__u8 *data = hid_bpf_get_data(hctx, 0 /* offset */, 9 /* size */);
+
+	if (!data)
+		return 0; /* EPERM check */
+
+	y = data[3] | (data[4] << 8);
+
+	y = -y;
+
+	data[3] = y & 0xFF;
+	data[4] = (y >> 8) & 0xFF;
+
+	return 0;
+}
+
+SEC("syscall")
+int probe(struct probe_args *ctx)
+{
+	/*
+	 * The device exports 3 interfaces.
+	 * The mouse interface has a report descriptor of length 71.
+	 * So if report descriptor size is not 71, mark as -EINVAL
+	 */
+	ctx->retval = ctx->rdesc_size != 71;
+	if (ctx->retval)
+		ctx->retval = -22;
+
+	/* comment the following line to actually bind the program */
+	ctx->retval = -22;
+
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
