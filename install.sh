8ec7472c049fd2f7fa0a3e97a805f48bbc583196
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2023-04-04 18:32:40 +0200

    Add a udev rule
    Install with
    ```
    cargo install --path . && \
    sudo cp ~/.cargo/bin/udev-hid-bpf /usr/local/bin/ && \
    sudo mkdir -p /lib/firmware/hid/bpf && \
    sudo cp target/bpf/*.bpf.o /lib/firmware/hid/bpf && \
    sudo cp 99-hid-bpf.rules /etc/udev/rules.d && \
    sudo udevadm control --reload
    ```
    
    Note that the bpf compiled programs are now ignored
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+if [ "$EUID" -ne 0 ]
+then
+  echo "This script needs to install global udev rules, so please run as root."
+  exit 1
+fi
+
+set -ex
+
+CARGO_USER=${SUDO_USER:-root}
+
+# Note: installing to a different prefix requires changes to the udev rule
+PREFIX=${1:-/usr/local}
+TMP_INSTALL_DIR="$SCRIPT_DIR/target/install"
+sudo -u $CARGO_USER -i cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
+
+install -D -t "$PREFIX"/bin/ "$TMP_INSTALL_DIR"/bin/udev-hid-bpf
+
+install -D -t /lib/firmware/hid/bpf target/bpf/*.bpf.o
+install -D -t /etc/udev/rules.d 99-hid-bpf.rules
+udevadm control --reload
