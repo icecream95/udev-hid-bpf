1b840b1a614b3c51702a422c79e3f2ef124f2b5a
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2023-04-04 10:45:06 +0200

    Remove bump_memlock_rlimit() call
    libbpf already does it by default now on old kernels that require it,
    but given that we need at least a v6.3 kernel, this is definitely
    not required.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,6 @@ use std::io;
 pub mod bpf;
 pub mod hidudev;
 
-fn bump_memlock_rlimit() -> Result<(), io::Error> {
-    let rlimit = libc::rlimit {
-        rlim_cur: 128 << 20,
-        rlim_max: 128 << 20,
-    };
-    if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } != 0 {
-        panic!("Failed to increase rlimit");
-    }
-    Ok(())
-}
-
 fn print_event(event: &udev::Event) {
     println!(
         "{}: {} {} (subsystem={}, sysname={})",
@@ -28,8 +17,6 @@ fn print_event(event: &udev::Event) {
 }
 
 fn main() -> Result<(), io::Error> {
-    bump_memlock_rlimit()?;
-
     let skel = bpf::HidBPF::open_and_load(false).expect("Could not load base eBPF program");
 
     hidudev::poll(skel, |x| print_event(x))
