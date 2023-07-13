9c53682e226ef6e06c9ae608e8e15ffe23bed999
Peter Hutterer <peter.hutterer@who-t.net>
2023-07-14 08:53:16 +1000

    install: fix a shellcheck complaint
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ CARGO_USER=${SUDO_USER:-root}
 # Note: installing to a different prefix requires changes to the udev rule
 PREFIX=${1:-/usr/local}
 TMP_INSTALL_DIR="$SCRIPT_DIR/target/install"
-sudo -u $CARGO_USER -i cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
+sudo -u "$CARGO_USER" -i cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
 
 install -D -t "$PREFIX"/bin/ "$TMP_INSTALL_DIR"/bin/udev-hid-bpf
 
