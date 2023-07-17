4d73226b72802fd6ff8dfdfbde100815a901152e
Peter Hutterer <peter.hutterer@who-t.net>
2023-07-17 12:05:27 +1000

    hidudev: switch to a Modalias class
    This is a better abstraction so we one place where we care about the
    format of the modalias string and the rest can just use the actual
    numbers to re-compose what they want.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/hidudev.rs
+++ src/hidudev.rs
@@ -8,6 +8,66 @@ pub struct HidUdev {
     udev_device: udev::Device,
 }
 
+pub struct Modalias {
+    bus: u32,
+    group: u32,
+    vid: u32,
+    pid: u32,
+}
+
+impl Modalias {
+    pub fn from_str(modalias: &str) -> std::io::Result<Self> {
+        /* strip out the "hid:" prefix from the modalias */
+        let modalias = modalias.trim_start_matches("hid:");
+
+        if modalias.len() != 28 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Invalid modalias '{}'", modalias),
+            ));
+        }
+
+        let econvert = |_| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Invalid modalias '{}'", modalias),
+            )
+        };
+
+        let bus = u32::from_str_radix(&modalias[1..5], 16).map_err(econvert)?;
+        let group = u32::from_str_radix(&modalias[6..10], 16).map_err(econvert)?;
+        let vid = u32::from_str_radix(&modalias[11..19], 16).map_err(econvert)?;
+        let pid = u32::from_str_radix(&modalias[20..28], 16).map_err(econvert)?;
+
+        Ok(Self {
+            bus,
+            group,
+            vid,
+            pid,
+        })
+    }
+
+    pub fn from_static_str(modalias: &'static str) -> std::io::Result<Self> {
+        Self::from_str(&modalias)
+    }
+
+    pub fn from_udev_device(udev_device: &udev::Device) -> std::io::Result<Self> {
+        let modalias = udev_device.property_value("MODALIAS");
+
+        let modalias = match modalias {
+            Some(data) => data,
+            _ => std::ffi::OsStr::new("hid:empty"), //panic!("modalias is empty"),
+        };
+
+        let modalias = match modalias.to_str() {
+            Some(data) => data,
+            _ => panic!("modalias problem"),
+        };
+
+        Self::from_str(modalias)
+    }
+}
+
 impl HidUdev {
     pub fn from_syspath(syspath: std::path::PathBuf) -> std::io::Result<Self> {
         let mut device = udev::Device::from_syspath(syspath.as_path())?;
@@ -39,24 +99,8 @@ impl HidUdev {
         })
     }
 
-    pub fn modalias(&self) -> String {
-        let modalias = self.udev_device.property_value("MODALIAS");
-
-        let modalias = match modalias {
-            Some(data) => data,
-            _ => std::ffi::OsStr::new("hid:empty"), //panic!("modalias is empty"),
-        };
-
-        let modalias = match modalias.to_str() {
-            Some(data) => data,
-            _ => panic!("modalias problem"),
-        };
-
-        /* strip out the "hid:" prefix from the modalias */
-        String::from(modalias)
-            .trim_start_matches("hid:")
-            .replace("v0000", "v")
-            .replace("p0000", "p")
+    pub fn modalias(&self) -> Modalias {
+        Modalias::from_udev_device(&self.udev_device).unwrap()
     }
 
     pub fn sysname(&self) -> String {
@@ -77,21 +121,11 @@ impl HidUdev {
             return Ok(());
         }
 
-        let prefix = self.modalias();
-
-        if prefix.len() != 20 {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("invalid modalias {} for device {}", prefix, self.sysname()),
-            ));
-        }
+        let modalias = self.modalias();
 
         let glob_path = bpf_dir.join(format!(
-            "b{{{},\\*}}g{{{},\\*}}v{{{},\\*}}p{{{},\\*}}*.bpf.o",
-            &prefix[1..5],
-            &prefix[6..10],
-            &prefix[11..15],
-            &prefix[16..20],
+            "b{{{:04X},\\*}}g{{{:04X},\\*}}v{{{:04X},\\*}}p{{{:04X},\\*}}*.bpf.o",
+            modalias.bus, modalias.group, modalias.vid, modalias.pid,
         ));
 
         let globset = GlobBuilder::new(glob_path.as_path().to_str().unwrap())
@@ -134,5 +168,46 @@ impl HidUdev {
         std::fs::remove_dir_all(path).ok();
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_modalias() {
+        let modalias = "b0003g0001v000004D9p0000A09F";
+        let m = Modalias::from_static_str(modalias);
+        assert!(m.is_ok());
+        let m = m.unwrap();
+        assert!(m.bus == 0x0003);
+        assert!(m.group == 0x0001);
+        assert!(m.vid == 0x04d9);
+        assert!(m.pid == 0xa09f);
+
+        // parsing doesn't care about uppercase hex
+        let m = Modalias::from_str(modalias.to_lowercase().as_str());
+        assert!(m.is_ok());
+        let m = m.unwrap();
+        assert!(m.bus == 0x0003);
+        assert!(m.group == 0x0001);
+        assert!(m.vid == 0x04d9);
+        assert!(m.pid == 0xa09f);
+
+        // 4-digit vid
+        let modalias = "b0003g0001v04D9p0000A09F";
+        let m = Modalias::from_str(modalias.to_lowercase().as_str());
+        assert!(m.is_err());
+
+        // 4-digit pid
+        let modalias = "b0003g0001v000004D9pA09F";
+        let m = Modalias::from_str(modalias.to_lowercase().as_str());
+        assert!(m.is_err());
+
+        // invalid char
+        let modalias = "b0003g0001v0000g4D9pA09F";
+        let m = Modalias::from_str(modalias.to_lowercase().as_str());
+        assert!(m.is_err());
     }
 }
