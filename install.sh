bf3cfac487f91c16ccd6fb40bf4942867756742a
Peter Hutterer <peter.hutterer@who-t.net>
2023-07-21 10:06:26 +1000

    install: adjust the PREFIX for our binary if necessary
    If we end up installing to a different prefix, adjust the udev rule
    accordingly.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- install.sh
+++ install.sh
@@ -12,15 +12,16 @@ set -ex
 
 CARGO_USER=${SUDO_USER:-root}
 
-# Note: installing to a different prefix requires changes to the udev rule
 PREFIX=${1:-/usr/local}
 TMP_INSTALL_DIR="$SCRIPT_DIR/target/install"
 sudo -u "$CARGO_USER" -i cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
 
 install -D -t "$PREFIX"/bin/ "$TMP_INSTALL_DIR"/bin/udev-hid-bpf
 
+sed -e "s|/usr/local|$PREFIX|" 99-hid-bpf.rules > target/bpf/99-hid-bpf.rules
+
 install -D -t /lib/firmware/hid/bpf target/bpf/*.bpf.o
-install -D -m 644 -t /etc/udev/rules.d 99-hid-bpf.rules
+install -D -m 644 -t /etc/udev/rules.d target/bpf/99-hid-bpf.rules
 install -D -m 644 -t /etc/udev/hwdb.d target/bpf/99-hid-bpf.hwdb
 udevadm control --reload
 systemd-hwdb update
