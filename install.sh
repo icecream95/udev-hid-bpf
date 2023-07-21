78a1935f6b07e1fc3728ef76491f93402e86bec7
Peter Hutterer <peter.hutterer@who-t.net>
2023-07-21 10:32:40 +1000

    install: pass the CARGO_TARGET_DIR through
    If for some reason that one is set in the environment, make sure we
    honour it and pass it through correctly.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- install.sh
+++ install.sh
@@ -36,19 +36,22 @@ done
 set -e
 
 CARGO_USER=${SUDO_USER:-root}
+CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-$SCRIPT_DIR/target}
 
 PREFIX=${1:-/usr/local}
-TMP_INSTALL_DIR="$SCRIPT_DIR/target/install"
-sudo -u "$CARGO_USER" -i cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
+TMP_INSTALL_DIR="$CARGO_TARGET_DIR/install"
 
-sed -e "s|/usr/local|$PREFIX|" 99-hid-bpf.rules > target/bpf/99-hid-bpf.rules
+sudo -u "$CARGO_USER" -i CARGO_TARGET_DIR="$CARGO_TARGET_DIR" \
+  cargo install --force --path "$SCRIPT_DIR" --root "$TMP_INSTALL_DIR" --no-track
+
+sed -e "s|/usr/local|$PREFIX|" 99-hid-bpf.rules > "$CARGO_TARGET_DIR"/bpf/99-hid-bpf.rules
 
 if [[ -z "$DRY_RUN" ]];
 then
   install -D -t "$PREFIX"/bin/ "$TMP_INSTALL_DIR"/bin/udev-hid-bpf
-  install -D -t /lib/firmware/hid/bpf target/bpf/*.bpf.o
-  install -D -m 644 -t /etc/udev/rules.d target/bpf/99-hid-bpf.rules
-  install -D -m 644 -t /etc/udev/hwdb.d target/bpf/99-hid-bpf.hwdb
+  install -D -t /lib/firmware/hid/bpf "$CARGO_TARGET_DIR"/bpf/*.bpf.o
+  install -D -m 644 -t /etc/udev/rules.d "$CARGO_TARGET_DIR"/bpf/99-hid-bpf.rules
+  install -D -m 644 -t /etc/udev/hwdb.d "$CARGO_TARGET_DIR"/bpf/99-hid-bpf.hwdb
   udevadm control --reload
   systemd-hwdb update
 fi
