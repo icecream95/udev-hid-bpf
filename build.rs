icecream95/udev-hid-bpf

commit dd7307524719f1440265a5a830c14d4fc7486dfd

build.rs: make our target_dir variable read-only
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

build.rs
@@ -85,13 +85,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
     let cargo_target_dir = env::var("CARGO_TARGET_DIR").unwrap_or(String::from("./target"));
-    let mut target_dir = PathBuf::from(cargo_target_dir);
-    target_dir.push(TARGET_DIR);
+    let target_dir = PathBuf::from(cargo_target_dir).join(TARGET_DIR);
 
     std::fs::create_dir_all(target_dir.as_path())
         .expect(format!("Can't create TARGET_DIR '{}'", TARGET_DIR).as_str());
 
-    let hwdb_file = target_dir.clone().join("99-hid-bpf.hwdb");
+    let hwdb_file = target_dir.join("99-hid-bpf.hwdb");
     let hwdb_fd = File::create(hwdb_file)?;
 
     let mut modaliases = std::collections::HashMap::new();

commit 71abfbaba39847210c9c2dcb7c89b4ee86e30d81

build.rs: don't clone the target_dir
This hunk got lost in a rebase and was missing from dd7307524719

Fixes: dd7307524719 ("build.rs: make our target_dir variable read-only")

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

build.rs
@@ -112,7 +112,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Then compile all other .bpf.c in a .bpf.o file
     for subdir in &["testing", "stable", "userhacks"] {
         let hwdb_name = format!("99-hid-bpf-{}.hwdb", subdir);
-        let hwdb_file = target_dir.clone().join(hwdb_name);
+        let hwdb_file = target_dir.join(hwdb_name);
         let hwdb_fd = File::create(hwdb_file)?;
 
         let mut modaliases = std::collections::HashMap::new();