3157c9b39e94637230937863fd2b2fc8c29e5768
Peter Hutterer <peter.hutterer@who-t.net>
2024-04-02 11:49:00 +1000

    CI: add a ruff format job
    This is currently unused but we'll add a Python tool soon so let's get
    this in place and reformat our currently only python file to match
    ruff's expectations.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'udev-hid-bpf'
-copyright = '2023, Benjamin Tissoires'
-author = 'Benjamin Tissoires'
+project = "udev-hid-bpf"
+copyright = "2023, Benjamin Tissoires"
+author = "Benjamin Tissoires"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -17,13 +17,12 @@ extensions = [
     "myst_parser",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
