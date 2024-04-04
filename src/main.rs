icecream95/udev-hid-bpf

commit 630376b097dd3baaae9329cff7e55655d746d596

Only consider bpf.o files in $prefix/lib/firmware/hid/bpf
Instead of hardcoding /usr and /usr/local let's work with the
meson-provided bpfdir (from the prefix) and use that as our only default
lookup path.

This means installing in different prefixes will work independently but
that's kinda the point. If there's a use-case for multiple lookup
directories we can sort this out then.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/main.rs
@@ -10,7 +10,7 @@ pub mod hidudev;
 pub mod modalias;
 
 // FIXME: how can we make this configurable?
-static DEFAULT_BPF_DIRS: &[&str] = &["/usr/local/lib/firmware/hid/bpf", "/lib/firmware/hid/bpf"];
+static DEFAULT_BPF_DIRS: &[&str] = &[env!("BPF_INSTALL_DIR")];
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]

build.rs
@@ -118,6 +118,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("cargo:warning=############################################################");
     }
 
+    // cargo install will break but meh
+    let bpf_install_dir =
+        env::var("BPF_INSTALL_DIR").unwrap_or(String::from("/lib/firmare/hid/bpf"));
+    println!("cargo:rustc-env=BPF_INSTALL_DIR={bpf_install_dir}");
+
     let source_root = env::var("BPF_SOURCE_ROOT").unwrap_or(String::from("."));
     let bpf_src_dir = PathBuf::from(source_root).join(BPF_SOURCE_DIR);
     println!("cargo:rerun-if-changed={}", bpf_src_dir.display());

meson.build
@@ -60,6 +60,7 @@ cargo_env = {
     'CARGO_TARGET_DIR': meson.project_build_root(),
     'BPF_SOURCE_ROOT': meson.project_source_root(),
     'EXTRA_INCLUDE': meson.project_source_root() / 'src' / 'bpf',
+    'BPF_INSTALL_DIR': bpfdir,
 }
 
 cargo_files = files(

commit ce4748193e256d8aeaa405a2999034e9c77c3ec2

Remove a leftover now-obsolete FIXME
Fixes: 630376b097dd ("Only consider bpf.o files in $prefix/lib/firmware/hid/bpf")

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/main.rs
@@ -9,7 +9,6 @@ pub mod bpf;
 pub mod hidudev;
 pub mod modalias;
 
-// FIXME: how can we make this configurable?
 static DEFAULT_BPF_DIRS: &[&str] = &[env!("BPF_INSTALL_DIR")];
 
 #[derive(Parser, Debug)]