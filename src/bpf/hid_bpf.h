e5ae6773e3ab662c8a2cd3f1d7f04e7087e99a10
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2024-05-15 18:30:28 +0200

    bpf: import new stable bpf programs
    They are now using a struct_ops instead of tracing, so some changes are
    required in udev-hid-bpf
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- src/bpf/hid_bpf.h
+++ src/bpf/hid_bpf.h
@@ -5,8 +5,17 @@
 #ifndef ____HID_BPF__H
 #define ____HID_BPF__H
 
+#ifndef HID_BPF_TRACING
+#define HID_BPF_DEVICE_EVENT "struct_ops/hid_device_event"
+#define HID_BPF_RDESC_FIXUP  "struct_ops/hid_rdesc_fixup"
+#define HID_BPF_OPS(name) SEC(".struct_ops.link") \
+	struct hid_bpf_ops name
+#else
 #define HID_BPF_DEVICE_EVENT "fmod_ret/hid_bpf_device_event"
 #define HID_BPF_RDESC_FIXUP  "fmod_ret/hid_bpf_rdesc_fixup"
+#define HID_BPF_OPS(name) \
+	struct hid_bpf_ops name
+#endif
 
 struct hid_bpf_probe_args {
 	unsigned int hid;
