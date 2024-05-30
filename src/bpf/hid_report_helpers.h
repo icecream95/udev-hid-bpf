icecream95/udev-hid-bpf

commit e9522cd0bff9e160c414475a609b381d9b614d79

Add a HID report composition helper macros
These macros make it slightly easier and more modular to create
a HID report descriptor from scratch. Since they carry the annotation
we don't need to comment it and they cannot get stale.

For comparison, before we had this:

	0x15, 0x00,                    //   Logical Minimum (0)
	0x25, 0x01,                    //   Logical Maximum (1)
	0x95, 0x04,                    //   Report Count (4)
	0x75, 0x01,                    //   Report Size (1)

Now we can write this as:
        LogicalRange_i8(0, 1)
        ReportCount(4)
        ReportSize(1)

Because these macros are for creating new report descriptors,
some bits aren't directly exposed. e.g in the example above:
there is a logical range as one macro that sets both min and max.
There is seldom a good use case for skipping either anyway.

These macros will need to be expanded over time.

For Usage Pages and Usage IDs, we use a tool to parse the HUT JSON (attached
to the HUT 1.5 PDF [1]) and generate all #defines for all usage pages
and usages in the form:

 #define UsagePage_Foo_Bar
 #define Usage_FB_SomeOrOther

Where the FB is simply the acronym based on the capital letters in the
Usage Page name or the first three letters, whichever makes slightly
more sense.

[1] https://usb.org/document-library/hid-usage-tables-15

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

tools/hid_report_helpers.h.jinja
@@ -1,0 +1,135 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright (c) 2024 Red Hat, Inc
+ */
+
+// THIS FILE IS GENERATED, DO NOT EDIT
+{# Ignore the warning above, you're looking at the source file #}
+
+#pragma once
+
+
+/* Macros for composing HID reports.
+ *
+ * HID Fields are added manually to the template, please add to it as needed
+ * for any individual device. The Usage Pages and Usages are generated.
+ *
+ * Some macros have a _i8, _i16, or _i32 suffix. Pick the
+ * right suffix given the passed-in value.
+ */
+
+/*
+ * This macro behaves like static_assert(), failing to
+ * compile if its argument is not true.  However, it always
+ * returns 0, which allows using it everywhere an expression
+ * can be used.
+ */
+#define must_be(e, msg_)                                  \
+(                                                         \
+    0 * (int) sizeof(                                     \
+        struct {                                          \
+            _Static_assert(e, msg_);                      \
+            int  ISO_C_forbids_a_struct_with_no_members;  \
+        }                                                 \
+    )                                                     \
+)
+
+/* Ensure the given value fits within 8/16/32 bits */
+#define i4(v_)  (((__u8)(v_) & 0xf)  + must_be((v_) >= -0x8 && (v_) <= 0x7, "not a i4"))
+#define i8(v_)  ((__u8)(v_)  + must_be((v_) >= -0x80 && (v_) <= 0xff, "not a i8/u8"))
+#define i16(v_) ((__u16)(v_) + must_be((v_) >= -0x8000 && (v_) <= 0xffff, "not a i16/u16"))
+#define i32(v_) ((__u32)(v_) + must_be((v_) >= -0x80000000L && (v_) <= 0xffffffffL, "not a i32/u16"))
+
+/* Split a value across multiple bytes in LE order */
+#define LE16(v_) i16(v_) & 0xff, ((v_) >> 8) & 0xff
+#define LE32(v_) i32(v_) & 0xff, ((v_) >> 8) & 0xff, ((v_) >> 16) & 0xff, ((v_) >> 24) & 0xff
+
+/* Collections require two items in the report descriptor, the start
+ * of the collection (0xa?) and the EndCollection item (0xc?).
+ * This macro provides both, use like this:
+ *
+ * static const __u8 fixed_rdesc[] = {
+ *     UsagePage_Generic_Desktop
+ *     Usage_GD_Keyboard
+ *     CollectionApplication(     ← Open the collection
+ *         ReportId(3)
+ *         LogicalRange_i8(0, 1)
+ *         // other fields
+ *     )                          ← End EndCollection
+ *
+ * Collections may be nested.
+ */
+#define Collection(col_, ...)		0xa1, i8(col_), __VA_ARGS__ 0xc0,
+#define CollectionPhysical(...)		Collection(0x00, __VA_ARGS__)
+#define CollectionApplication(...)	Collection(0x01, __VA_ARGS__)
+#define CollectionLogical(...)		Collection(0x02, __VA_ARGS__)
+
+/* See Collections, this macro provides Push and Pop with
+ * elements in between */
+#define PushPop(...)			0xa4, __VA_ARGS__ 0xb4,
+
+/* Arguments to use in bitwise-or for Input, Output, Feature */
+#define Const		0x1
+#define Var		0x2
+#define Arr		0x0
+#define Abs		0x0
+#define Rel		0x4
+
+/* Use like this: Input(Var|Abs) */
+#define Input(i_)			0x081, i8(i_),
+#define Output(i_)			0x091, i8(i_),
+#define Feature(i_)			0x0b1, i8(i_),
+
+#define ReportId(id_)			0x85, i8(id_),
+#define ReportSize(sz_)		        0x75, i8(sz_),
+#define ReportCount(cnt_)		0x95, i8(cnt_),
+
+#define LogicalRange_i8(min_, max_)	0x15, i8(min_), 0x25, i8(max_),
+#define LogicalRange_i16(min_, max_)	0x16, LE16(min_), 0x26, LE16(max_),
+#define LogicalRange_i32(min_, max_)	0x17, LE32(min_), 0x27, LE32(max_),
+
+#define PhysicalRange_i8(min_, max_)	0x35, i8(min_), 0x45, i8(max_),
+#define PhysicalRange_i16(min_, max_)	0x36, LE16(min_), 0x46, LE16(max_),
+#define PhysicalRange_i32(min_, max_)	0x37, LE32(min_), 0x47, LE32(max_),
+
+#define UsageRange_i8(min_, max_)	0x19, i8(min_), 0x29, i8(max_),
+#define UsageRange_i16(min_, max_)	0x1a, LE16(min_), 0x2a, LE16(max_),
+
+#define UsagePage_i8(p_)		0x05, i8(p_),
+#define UsagePage_i16(p_)		0x06, LE16(p_),
+
+#define Usage_i8(u_)			0x09, i8(u_),
+#define Usage_i16(u_)			0x0a, LE16(u_),
+#define Usage_i32(u_)			0x0b, LE32(u_),
+
+#define SILinear			0x1
+#define SIRotation			0x2
+#define EnglishLinear			0x3
+#define EnglishRotation			0x4
+#define cm				(SILinear | (0x1 << 4))
+#define rad				(SIRotation | (0x1 << 4))
+#define deg				(EnglishRotation | (0x1 << 4))
+#define in				(EnglishLinear | (0x1 << 4))
+/* Use as Unit(cm) or Unit(rad) or similar.
+ * This macro currently defaults to exponent 1 only, so no
+ * cm^2 or others */
+#define Unit(u_)                        Unit_i8(u_)
+#define Unit_i8(u_)			0x65, i8(u_),
+#define Unit_i16(u_)			0x66, i16(u_),
+#define Unit_i32(u_)			0x67, i32(u_),
+
+#define UnitExponent(u_)		0x55, i4(u_),
+
+/* ----- Generated Usage Pages and Usages ------ */
+{% for usage_page in usage_pages %}
+{% set size = "i8" if usage_page.value < 0xff  else "i16" %}
+#define UsagePage_{{ usage_page.name.ljust(30)}} UsagePage_{{size}}(0x{{'%x' % usage_page.value}})
+{% endfor %}
+#define UsagePage_Vendor(u_)		UsagePage_i16((u_) + must_be(((u_) & 0xff00) == 0xff00, "not a 0xff00 vendor page")
+
+{% for usage_page in usage_pages %}
+{% for usage in usage_page.usages %}
+{% set size = "i8" if usage.value < 0xff  else "i16" %}
+{% set name = usage_page.acronym + "_" + usage.name %}
+#define Usage_{{ name.ljust(40)}} Usage_{{size}}(0x{{'%x' % usage.value}})
+{% endfor %}
+{% endfor %}

tools/generate-hid-report-helpers.py
@@ -1,0 +1,245 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+#
+# Helper script to convert the jinja file into our hut.rs file
+#
+# Use as:
+#     $ python3 ./tools/generate-hid-report-helpers.py \
+#                        --datafile data/HidUsageTables.json \
+#                        ./tools/hid_report_helpers.h.jinja > src/bpf/hid_report_helpers.h
+
+from pathlib import Path
+from dataclasses import dataclass
+import argparse
+import enum
+import os
+import re
+import sys
+import functools
+import json
+import jinja2
+import jinja2.environment
+
+
+def sanitize(s):
+    s = re.sub(r"[-‐\t: \\|/.\n,()]", "", s)
+    s = re.sub(r"[+]", "Plus", s)
+    s = re.sub(r"[-]", "Minus", s)
+    numbers = {
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        "0": "Zero",
+    }
+    s = functools.reduce(
+        lambda s, kv: re.sub(rf"^({kv[0]})", kv[1], s), numbers.items(), s
+    )
+    return s
+
+
+def sanitize_keyboard(s):
+    map = {
+        "!": "ExclamationMark",
+        "@": "At",
+        "/": "Slash",
+        "#": "Hash",
+        "$": "Dollar",
+        "%": "Percent",
+        "^": "Caret",
+        "&": "Ampersand",
+        "*": "Star",
+        "[": "OpenBracket",
+        "]": "CloseBracket",
+        "{": "OpenBrace",
+        "}": "CloseBrace",
+        "(": "OpenParenthesis",
+        ")": "CloseParenthesis",
+        "'": "SingleQuote",
+        '"': "DoubleQuote",
+        "<": "LessThan",
+        ">": "GreaterThan",
+        " and ": "And",
+        " ": "",
+        ",": "Comma",
+        "-": "Minus",
+        "+": "Plus",
+        "|": "Pipe",
+        "?": "QuestionMark",
+        "=": "Equals",
+        "\\": "Backslash",
+        "~": "Tilde",
+        ";": "Semicolon",
+        ":": "Colon",
+        ".": "Period",
+    }
+    s = functools.reduce(lambda s, kv: s.replace(kv[0], kv[1]), map.items(), s)
+
+    numbers = {
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+        "0": "Zero",
+    }
+    s = functools.reduce(
+        lambda s, kv: re.sub(rf"^({kv[0]})", kv[1], s), numbers.items(), s
+    )
+    return s
+
+
+class UsageType(enum.StrEnum):
+    LINEAR_CONTROL = "LC"
+    ON_OFF_CONTROL = "OOC"
+    MOMENTARY_CONTROL = "MC"
+    ONE_SHOT_CONTROL = "OSC"
+    RETRIGGER_CONTROL = "RTC"
+    SELECTOR = "Sel"
+    STATIC_VALUE = "SV"
+    STATIC_FLAG = "SF"
+    DYNAMIC_VALUE = "DV"
+    DYNAMIC_FLAG = "DF"
+    NAMED_ARRAY = "NAry"
+    APPLICATION_COLLECTION = "CA"
+    LOGICAL_COLLECTION = "CL"
+    PHYSICAL_COLLECTION = "CP"
+    USAGE_SWITCH = "US"
+    USAGE_MODIFIER = "UM"
+    BUFFERED_BYTES = "BufferedBytes"
+
+
+class UsagePageType(enum.StrEnum):
+    DEFINED = "Defined"
+    GENERATED = "Generated"
+
+
+@dataclass
+class UsageId:
+    printable: str  # The string as it was in the data file
+    name: str  # Name of the usage sanitized to be a valid identifier
+    value: int
+    usage_types: list[UsageType]
+
+
+@dataclass
+class UsagePage:
+    printable: str
+    name: str
+    acronym: str
+    value: int
+    usages: list[UsageId]
+    usage_page_type: UsagePageType
+    name_prefix: str | None
+
+
+def parse_usage_id(u) -> UsageId:
+    # Some of the strings have \_ in them for whatever reason...
+    printable = u["Name"].replace("\\", "")
+    name = sanitize(printable)
+    value = int(u["Id"])
+    types = [UsageType(k) for k in u["Kinds"]]
+    usage = UsageId(printable=printable, name=name, value=value, usage_types=types)
+    return usage
+
+
+def parse_usage_page(up) -> UsagePage:
+    printable = up["Name"]
+    name = sanitize(printable)
+    acronym = "".join([x for x in name if x.isupper()])
+    if len(acronym) == 1:
+        acronym = name[:3]
+    value = int(up["Id"])
+    usages = sorted([parse_usage_id(u) for u in up["UsageIds"]], key=lambda u: u.value)
+    uptype = UsagePageType(up["Kind"])
+    name_prefix = None
+    if uptype == UsagePageType.GENERATED:
+        name_prefix = up["UsageIdGenerator"]["NamePrefix"]
+        if name_prefix.lower() == "enum":
+            name_prefix += "erate"
+    usage_page = UsagePage(
+        printable=printable,
+        name=name,
+        acronym=acronym,
+        value=value,
+        usages=usages,
+        usage_page_type=uptype,
+        name_prefix=name_prefix,
+    )
+    return usage_page
+
+
+def parse_data_file(datadir):
+    js = json.load(open(datadir, "r"))
+    usage_pages = [parse_usage_page(up) for up in js["UsagePages"]]
+
+    # For some reason the Unicode usage page isn't in the JSON but
+    # it is described on page 213 of the HUT 1.5 document. Let's insert
+    # it manually.
+    unicode_page = UsagePage(
+        printable="Unicode",
+        name="Unicode",
+        acronym="UC",
+        value=0x10,
+        usages=[],
+        usage_page_type=UsagePageType.GENERATED,
+        name_prefix="codepoint",
+    )
+    usage_pages.append(unicode_page)
+
+    return sorted(usage_pages, key=lambda up: up.value)
+
+
+def generate_source(
+    usage_pages: list[UsagePage], template: str
+) -> jinja2.environment.TemplateStream:
+    data = {}
+    data["usage_pages"] = usage_pages
+
+    loader: jinja2.BaseLoader
+    if template == "-":
+        loader = jinja2.FunctionLoader(lambda _: sys.stdin.read())
+        filename = "<stdin>"
+    else:
+        path = Path(template)
+        assert path.exists(), f"Failed to find template {path}"
+        filename = path.name
+        loader = jinja2.FileSystemLoader(os.fspath(path.parent))
+
+    env = jinja2.Environment(
+        loader=loader,
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    jtemplate = env.get_template(filename)
+    return jtemplate.stream(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", default="-")
+    parser.add_argument(
+        "--datafile",
+        type=Path,
+        default=Path("data"),
+        help="Path to the HUT json",
+    )
+    parser.add_argument("template", type=Path, help="The jinja template file")
+    args = parser.parse_args()
+    assert args.datafile.exists()
+    assert args.template.exists()
+
+    hut = parse_data_file(args.datafile)
+
+    stream = generate_source(usage_pages=hut, template=args.template)
+    file = sys.stdout if args.output == "-" else open(args.output, "w")
+    stream.dump(file)

commit 2b1ccd93ec65826705e083a7063f0f7a0e615a53

bpf: add missing closing parens to UsagePage_Vendor
Fixes: e9522cd0bff9 ("Add a HID report composition helper macros")

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/bpf/hid_report_helpers.h
@@ -153,7 +153,7 @@
 #define UsagePage_CameraControl                  UsagePage_i8(0x90)
 #define UsagePage_Arcade                         UsagePage_i8(0x91)
 #define UsagePage_FIDOAlliance                   UsagePage_i16(0xf1d0)
-#define UsagePage_Vendor(u_)		UsagePage_i16((u_) + must_be(((u_) & 0xff00) == 0xff00, "not a 0xff00 vendor page")
+#define UsagePage_Vendor(u_)		UsagePage_i16((u_) + must_be(((u_) & 0xff00) == 0xff00, "not a 0xff00 vendor page"))
 
 #define Usage_GD_Pointer                               Usage_i8(0x1)
 #define Usage_GD_Mouse                                 Usage_i8(0x2)

tools/hid_report_helpers.h.jinja
@@ -124,7 +124,7 @@
 {% set size = "i8" if usage_page.value < 0xff  else "i16" %}
 #define UsagePage_{{ usage_page.name.ljust(30)}} UsagePage_{{size}}(0x{{'%x' % usage_page.value}})
 {% endfor %}
-#define UsagePage_Vendor(u_)		UsagePage_i16((u_) + must_be(((u_) & 0xff00) == 0xff00, "not a 0xff00 vendor page")
+#define UsagePage_Vendor(u_)		UsagePage_i16((u_) + must_be(((u_) & 0xff00) == 0xff00, "not a 0xff00 vendor page"))
 
 {% for usage_page in usage_pages %}
 {% for usage in usage_page.usages %}