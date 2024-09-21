8975469b76930dc074c956dd398eced0c671b3fd
Alexander Olofsson <ace@haxalot.com>
2024-09-21 10:10:01 +0200

    bpf/userhack: Add MX Master 3S horizontal scroll fix
    The Logitech MX Master 3S has its thumb wheel set up the opposite
    direction to the other mice in the MX Master lineup, so invert it to
    keep things consistent.
    
    Signed-off-by: Alexander Olofsson <me@ananace.dev>

---
+++ src/bpf/userhacks/0010-Logitech-MX-Master-3S-horiz-scroll.bpf.c
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "vmlinux.h"
+#include "hid_bpf.h"
+#include "hid_bpf_helpers.h"
+#include <bpf/bpf_tracing.h>
+
+#define VID_LOGITECH			0x046D
+#define PID_BOLT_RECEIVER		0xC548
+#define PID_MX_MASTER_3S_BLUETOOTH	0xB028
+
+HID_BPF_CONFIG(
+	HID_DEVICE(BUS_USB, HID_GROUP_GENERIC, VID_LOGITECH, PID_BOLT_RECEIVER);
+);
+
+#define MASTER3S_REPORT_DESCRIPTOR_LENGTH 54
+
+/*
+ * This program inverts the horizontal scroll direction on
+ * the MX Master 3S mouse.
+ * Out of the box it outputs inverse values compared to
+ * what's given by other mice.
+ *
+ * Only implemented for the Logitech Bolt receiever at the
+ * moment, bluetooth might be handled by libinput already.
+ */
+
+struct master_3s_hid_report {
+	__u8 report_id;
+	__u16 buttons;
+	__s16 x;
+	__s16 y;
+	__s8 wheel;
+	__s8 pan;
+} __attribute__((packed));
+
+SEC(HID_BPF_DEVICE_EVENT)
+int BPF_PROG(master_3s_fix_event, struct hid_bpf_ctx *hid_ctx)
+{
+	const int expected_report_id = 2;
+
+	struct master_3s_hid_report *data = (struct master_3s_hid_report*)hid_bpf_get_data(hid_ctx, 0, sizeof(*data));
+	if (!data || data->report_id != expected_report_id)
+		return 0; /* EPERM check */
+
+#if false
+	bpf_printk(" ** HID Report:");
+	bpf_printk("    buttons: %04x", data->buttons);
+	bpf_printk("    x: %i", data->x);
+	bpf_printk("    y: %i", data->y);
+	bpf_printk("    wheel: %i", data->wheel);
+	bpf_printk("    pan: %i", data->pan);
+#endif
+
+	// Invert the horizontal scroll direction
+	data->pan = -data->pan;
+
+	return sizeof(*data);
+}
+
+HID_BPF_OPS(master_3s) = {
+	.hid_device_event = (void *)master_3s_fix_event,
+};
+
+SEC("syscall")
+int probe(struct hid_bpf_probe_args *ctx)
+{
+	ctx->retval = ctx->rdesc_size != MASTER3S_REPORT_DESCRIPTOR_LENGTH;
+	if (ctx->retval)
+		ctx->retval = -EINVAL;
+
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
