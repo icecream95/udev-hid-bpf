b54e916b630ccafff08a13cc543f183e5989824f
Alexander Olofsson <me@ananace.dev>
2024-11-06 10:46:05 +0100

    bpf/userhacks: Fix probe for MX Master 3S
    The bolt receiver looks to be a little unstable in the report
    descriptor department, so selecting based on descriptor content instead
    of length seems to be necessary for guaranteed functionality across
    firmware updates.
    
    Signed-off-by: Alexander Olofsson <me@ananace.dev>
    
    Minor style fix
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Part-of: <https://gitlab.freedesktop.org/libevdev/udev-hid-bpf/-/merge_requests/157>

--- src/bpf/userhacks/0010-Logitech-MX-Master-3S-horiz-scroll.bpf.c
+++ src/bpf/userhacks/0010-Logitech-MX-Master-3S-horiz-scroll.bpf.c
@@ -3,17 +3,16 @@
 #include "vmlinux.h"
 #include "hid_bpf.h"
 #include "hid_bpf_helpers.h"
+#include "hid_report_helpers.h"
 #include <bpf/bpf_tracing.h>
 
 #define VID_LOGITECH			0x046D
 #define PID_BOLT_RECEIVER		0xC548
-#define PID_MX_MASTER_3S_BLUETOOTH	0xB028
+//      PID_MX_MASTER_3S_BLUETOOTH	0xB028
 
 HID_BPF_CONFIG(
 	HID_DEVICE(BUS_USB, HID_GROUP_GENERIC, VID_LOGITECH, PID_BOLT_RECEIVER);
 );
-
-#define MASTER3S_REPORT_DESCRIPTOR_LENGTH 54
 
 /*
  * This program inverts the horizontal scroll direction on
@@ -37,9 +36,14 @@ struct master_3s_hid_report {
 SEC(HID_BPF_DEVICE_EVENT)
 int BPF_PROG(master_3s_fix_event, struct hid_bpf_ctx *hid_ctx)
 {
+	const int expected_length = 9;
 	const int expected_report_id = 2;
+	struct master_3s_hid_report *data = NULL;
 
-	struct master_3s_hid_report *data = (struct master_3s_hid_report*)hid_bpf_get_data(hid_ctx, 0, sizeof(*data));
+	if (hid_ctx->size < expected_length)
+		return 0;
+
+	data = (struct master_3s_hid_report*)hid_bpf_get_data(hid_ctx, 0, sizeof(*data));
 	if (!data || data->report_id != expected_report_id)
 		return 0; /* EPERM check */
 
@@ -65,8 +69,15 @@ HID_BPF_OPS(master_3s) = {
 SEC("syscall")
 int probe(struct hid_bpf_probe_args *ctx)
 {
-	ctx->retval = ctx->rdesc_size != MASTER3S_REPORT_DESCRIPTOR_LENGTH;
-	if (ctx->retval)
+	const __u8 expected_rdesc[] = {
+		UsagePage_GenericDesktop
+		Usage_GD_Mouse
+	};
+
+	if (ctx->rdesc_size > sizeof(expected_rdesc) &&
+	    __builtin_memcmp(ctx->rdesc, expected_rdesc, sizeof(expected_rdesc)) == 0)
+		ctx->retval = 0;
+	else
 		ctx->retval = -EINVAL;
 
 	return 0;
